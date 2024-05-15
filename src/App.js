3c787248e57948e431300157a31512d6cd77e3a4
Jeffrey <jeferose4@gmail.com>
2024-05-14 21:15:16 -0600

    initialized git repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Discounted from './components/Discounted';
+import Featured from './components/Featured';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
+import Nav from './components/Nav'
+import Explore from './components/Explore'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
