a5f1b590d79b8aafdfe7d270f07f7267c951d224
Jeffrey <jeferose4@gmail.com>
2024-05-20 19:09:26 -0600

    made cart functional

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -1,13 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
 import Book from "../components/ui/Book";
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart, cart }) => {
   const { id } = useParams();
   const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find(book => book.id === +id);
+  }
 
   return (
     <div id="books__body">
@@ -52,7 +60,15 @@ const BookInfo = ({ books }) => {
                     reiciendis.
                   </p>
                 </div>
-                <button className="btn">Add to cart</button>
+                {bookExistsOnCart() ? (
+              <Link to={`/cart`} className="book__link">
+                  <button className="btn">Checkout</button>
+              </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to cart
+                  </button>
+                )}
               </div>
             </div>
           </div>
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React from "react";
+import EmptyCart from "../assets/empty_cart.svg";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const Cart = () => {
+const Cart = ({ cart, changeQuantity, removeItem }) => {
+  const calculateSubTotal = () => {
+    let price = 0;
+    cart.forEach((item) => {
+      price += +((item.salePrice || item.originalPrice) * item.quantity);
+    });
+    return price;
+  };
+
+  const subTotal = calculateSubTotal();
+  const tax = subTotal * 0.1;
+  const total = subTotal + tax;
+
   return (
     <div id="books__body">
       <main className="books__main">
@@ -11,16 +25,94 @@ const Cart = () => {
             </div>
             <div className="cart">
               <div className="cart__header">
-                <div className="cart__book">Book</div>
-                <div className="cart__quantity">Quantity</div>
-                <div className="cart__total">Price</div>
+                <span className="cart__book">Book</span>
+                <span className="cart__quantity">Quantity</span>
+                <span className="cart__total">Price</span>
               </div>
+              <div className="cart__body">
+                {cart.map((book) => {
+                  return (
+                    <div className="cart__item">
+                      <div className="cart__book">
+                        <img
+                          src={book.url}
+                          className="cart__book--img"
+                          alt=""
+                        />
+                        <div className="cart__book--info">
+                          <span className="cart__book--title">
+                            {book.title}
+                          </span>
+                          <span className="cart__book--price">
+                            ${(book.salePrice || book.originalPrice).toFixed(2)}
+                          </span>
+                          <button
+                            className="cart__book--remove"
+                            onClick={() => removeItem(book)}
+                          >
+                            Remove
+                          </button>
+                        </div>
+                      </div>
+                      <div className="cart__quantity">
+                        <input
+                          type="number"
+                          min={0}
+                          max={99}
+                          className="cart__input"
+                          value={book.quantity}
+                          onChange={(event) =>
+                            changeQuantity(book, event.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="cart__total">
+                        $
+                        {(
+                          (book.salePrice || book.originalPrice) * book.quantity
+                        ).toFixed(2)}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              {cart.length === 0 && (
+                <div className="cart__empty">
+                  <img src={EmptyCart} alt="" className="cart__empty--img" />
+                  <h2>You don't have any books in your cart</h2>
+                  <Link to="/books">
+                    <button className="btn">Browse Books</button>
+                  </Link>
+                </div>
+              )}
             </div>
+            {cart.length > 0 && (
+              <div className="total">
+                <div className="total__item total__sub-total">
+                  <span>Subtotal</span>
+                  <span>${subTotal.toFixed(2)}</span>
+                </div>
+                <div className="total__item total__tax">
+                  <span>Tax</span>
+                  <span>${tax.toFixed(2)}</span>
+                </div>
+                <div className="total__item total__price">
+                  <span>Total</span>
+                  <span>${total.toFixed(2)}</span>
+                </div>
+                <button
+                  className="btn btn__checkout no-cursor"
+                  onClick={() => alert(`Haven't got around to doing this :(`)}
+                >
+                  Proceed to checkout
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
